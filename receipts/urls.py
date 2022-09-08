4bc3d28644933859b861c6a3b54ecbaea2d72822
Ally <allygoose93@gmail.com>
2022-09-08 13:16:34 -0700

    Feature 5 completed

---
+++ receipts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from receipts.views import ReceiptListView
+
+urlpatterns = [
+    path("", ReceiptListView.as_view(), name="home"),
+]
--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+
+# from django.shortcuts import render
+from receipts.models import Receipt
 
 # Create your views here.
+class ReceiptListView(ListView):
+    model = Receipt
+    template_name = "receipts/list.html"
